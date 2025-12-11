fbac6d4121a652d717b44608962fe5b4ecc0f353
Caio Peixoto <caioluispeixotos@gmail.com>
2025-12-11 01:43:40 +0000

    adione esses pixels no site <!-- Meta Pixel Code --> <script> !function(

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Hellen Criativa',
@@ -21,6 +22,46 @@ export default function RootLayout({
         <script src="https://fast.wistia.com/player.js" async></script>
         <script src="https://fast.wistia.com/embed/n1pj3f7c1k.js" async type="module"></script>
         <style>{`wistia-player[media-id='n1pj3f7c1k']:not(:defined) { background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/n1pj3f7c1k/swatch'); display: block; filter: blur(5px); padding-top:133.33%; }`}</style>
+        
+        {/* Meta Pixel Code */}
+        <Script id="fb-pixel-script" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1921485951740158');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1921485951740158&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
+        {/* Utmify Pixel */}
+        <Script id="utmify-pixel-config" strategy="afterInteractive">
+          {`window.pixelId = "693a217a7130fff964d86bff";`}
+        </Script>
+        <Script
+          src="https://cdn.utmify.com.br/scripts/pixel/pixel.js"
+          strategy="afterInteractive"
+          async
+          defer
+        ></Script>
+        <Script
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          async
+          defer
+        ></Script>
       </head>
       <body className="font-body antialiased">
         {children}
